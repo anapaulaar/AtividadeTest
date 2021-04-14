be96b23c54b59b697f733b29166c73b3c19686eb
Ana Paula <almeidaribeiroanapaula@gmail.com>
2021-04-14 18:28:20 -0300

    adicionando produtos e listando

---
+++ lista-produtos.js
@@ -0,0 +1,40 @@
+var produtos = [
+{ 
+    nome: 'Amendoim',
+    preco: 1.99,
+    estoque: 5
+},
+{ 
+    nome: 'Paçoca',
+    preco: 0.99,
+    estoque: 10
+},
+{ 
+    nome: 'Cocada',
+    preco: 3.50,
+    estoque: 7
+},
+{ 
+    nome: 'Doce de leite',
+    preco: 1.99,
+    estoque: 15
+},
+{ 
+    nome: 'Doce de ninho',
+    preco: 1.99,
+    estoque: 0
+}
+]
+
+function filtrarEstoque (todosProdutos){
+    return todosProdutos.filter(produto => produto.estoque)
+}
+
+filtrarEstoque (produtos)
+    .forEach(produto => console.log(`Produtos com estoque: ${produto.nome} - preço: ${produto.preco}`));
+
+produtos.sort(function (a, b){
+    return a.preco - b.preco;
+});
+
+console.log(produtos)
